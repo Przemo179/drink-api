800aea5816193e7d16d3d94b00f9c1f5ed22887a
przemo179 <przemus17889@gmail.com>
2023-10-02 13:28:40 +0200

    init project

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+  db: {
+    /* don't expose password or any sensitive info, done only for demo */
+    host: "mysql.db.mdbgo.com",
+    user: "przemo179_premysl",
+    password: "7H4s.WWGi1",
+    database: "przemo179_drinkapidatabase",
+    connectTimeout: 60000
+  },
+};
+module.exports = config;
